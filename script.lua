3f9893b51247f76e86ebe904c1b4d4c43a729dcd
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-01-27 18:36:10 -0600

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/SKHub-Orion/main/Your%20hub%20men"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/SKHub-Orion/main/YesSKHub"))()
